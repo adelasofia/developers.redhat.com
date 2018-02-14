0f5e1cf2ba5507bfb457ace5afb01eb9d426f286
Adela Sofia Arreola <aarreola@redhat.com>
2018-02-14 11:26:26 -0700

    Added show more function to LP

--- javascripts/show-more.js
+++ javascripts/show-more.js
@@ -1,18 +1,42 @@
 var showMoreText = $('.show-more-text').text();
-
-// Minimum number of characters needed to display "Show More" button
 var charCount = $('.show-more-text').attr('data-count');
+var maxWidth = $('.show-more-text').attr('data-max-width');
 
 if (showMoreText.length < charCount){
   $('a.show-more').hide();
 }
 
-$('.show-more').on('click', function() {
 
+$('.show-more').on('click', function() {
   var x = $(this);
   var $showMoreBtn = x.find('span');
   var $showMoreContent = x.prev();
   $showMoreContent.toggleClass('open');
 
   return $showMoreBtn.toggle();
+});
+
+function screen_resize() {
+  var width = parseInt(window.innerWidth);
+
+  if ( maxWidth ) {
+
+    if ( width <= maxWidth ) {
+      $('a.show-more').show();
+      console.log("shown");
+      $('div.show-more-text').removeClass("auto-open");
+
+    } else {
+      $('a.show-more').hide();
+      $('div.show-more-text').addClass("auto-open");
+    }
+  }
+}
+
+$(window).resize(function(e) {
+  screen_resize();
+});
+
+$(document).ready(function(e) {
+  screen_resize();
 });
